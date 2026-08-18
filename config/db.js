9f2bb307247c3219acc58e7c6280efcb8026b1c7
gzaf5466 <gzaf5466@gmail.com>
2026-08-18 15:24:35 +0530

    Fix PrismaClient constructor argument

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient({ url: process.env.DATABASE_URL });
+const prisma = new PrismaClient();
 
 const connectDB = async () => {
   try {
